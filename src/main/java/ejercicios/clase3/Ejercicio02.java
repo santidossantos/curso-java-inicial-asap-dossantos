b818524a8f57c9ebaf2a5c33c725c9951f6431b6
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-07-12 14:56:07 -0300

    Ejercicio02 Clase 3

--- src/main/java/ejercicios/clase3/Ejercicio02.java
+++ src/main/java/ejercicios/clase3/Ejercicio02.java
@@ -9,6 +9,44 @@ el minimo y las posiciones que ocupan en el array.
 
  */
 
+import com.github.sanchezih.util.io.Consola;
+
 public class Ejercicio02 {
+
+    public static int calcularMaximo(int[] arreglo) {
+        int maximo = arreglo[0];
+        for(int i=1; i < arreglo.length; i++) {
+            if(arreglo[i] > maximo) {
+                maximo = arreglo[i];
+            }
+        }
+        return maximo;
+    }
+
+    public static int calcularMinimo(int[] arreglo) {
+        int minimo = arreglo[0];
+        for(int i=1; i < arreglo.length; i++) {
+            if(arreglo[i] < minimo) {
+                minimo = arreglo[i];
+            }
+        }
+        return minimo;
+    }
+
+    public static int[] leerArreglo() {
+        int[] numbers = new int[10];
+
+        for(int i=0; i < numbers.length; i++) {
+            numbers[i] = Consola.leerEntero("Ingrese un número: ");
+        }
+
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = leerArreglo();
+        System.out.println("El número máximo es: " + calcularMaximo(numbers));
+        System.out.println("El número mínimo es: " + calcularMinimo(numbers));
+    }
 
 }
