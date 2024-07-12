73cec737124158d0033b60311264dbb3faec6197
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-07-12 15:27:28 -0300

    Ejercicio03 Clase 3

--- src/main/java/ejercicios/clase3/Ejercicio03.java
+++ src/main/java/ejercicios/clase3/Ejercicio03.java
@@ -9,4 +9,23 @@ e indica por pantalla si dicho array es capicua.
 
 public class Ejercicio03 {
 
+    public static boolean isCapicua(int[] arreglo) {
+        int i = 0;
+        int j = arreglo.length - 1;
+
+        while (i < arreglo.length) {
+            if (arreglo[i] != arreglo[j]) {
+                return false;
+            }
+            i++; j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 2, 1};
+        System.out.println(isCapicua(array));
+    }
+
 }
